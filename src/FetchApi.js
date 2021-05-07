7fcb979b5062ce42023167cca187d53753a3460f
aayushadeshara333 <adeshara.aayush@gmail.com>
2021-05-07 11:36:18 +0530

    handled fetching

--- src/FetchApi.js
+++ src/FetchApi.js
@@ -1,21 +1,30 @@
 import useFetch from './useFetch';
 import Home from './Home';
+import { useState } from 'react';
 
 
 const FetchApi = () => {
     const API_KEY = process.env.REACT_APP_API_KEY;
     const URL = process.env.REACT_APP_URL;
-    const { data, isPending, error } = useFetch(`${URL}/live?access_key=${API_KEY}&format=1`);
+    let flag = false;
+    const { data, isPending, error } = useFetch(`${URL}/`);
+
+    if(data === null){
+        flag = true;
+        document.getElementsByTagName("title").innerHTML = "Hello";
+    }
+
     let arr = []
-    if (!isPending) {
+    if (!isPending && flag === false) {
         for (let e in data["quotes"]) {
             arr.push(e);
         }
     }
     return (
         <div>
+            {flag && <div className="home">Unable to fetch data</div>}
             {isPending && <div className="home">Loading</div>}
-            {!isPending && <Home arr={arr} data={data["quotes"]} error={error}/>}
+            {!isPending && flag === false && <Home arr={arr} data={data["quotes"]} error={error}/>}
         </div>
     );
 }
--- src/Table.js
+++ src/Table.js
@@ -6,7 +6,13 @@ const Table = () => {
     const API_KEY = process.env.REACT_APP_API_KEY;
     const URL = process.env.REACT_APP_URL;
     const { data, isPending, error } = useFetch(`${URL}/live?access_key=${API_KEY}&format=1`);
+    let flag = false;
     let arr = []
+
+    if (data === null || data.error.code === 101) {
+        flag = true;
+    }
+
     if (!isPending) {
         for (let e in data["quotes"]) {
             arr.push(e);
@@ -15,18 +21,22 @@ const Table = () => {
 
     const [selected, setSelected] = useState("USDINR");
     return (
-        <div className="table_content">
-            <h1 style={{ color: "rgba(255, 255, 255, 0.8)" }}>Currency Table</h1>
-            <table>
-                <tr>
-                    <th colSpan='2'>
-                        <select value={selected.slice(3, 6)} onChange={(e) => setSelected("USD" + e.target.value)}>
-                            {arr.map((ele) => { return <option>{ele.slice(3, 6)}</option> })}
-                        </select>
-                    </th>
-                </tr>
-                {arr.map((e) => { return <tr><td>{e.slice(3, 6)}</td><td>{(data.quotes[e] / data.quotes[selected]).toFixed(5)}</td></tr> })}
-            </table>
+        <div>
+            {flag && <div className="home">Unable to fetch data</div>}
+            {isPending && <div className="home">Loading</div>}
+            {!isPending && flag === false && <div className="table_content">
+                <h1 style={{ color: "rgba(255, 255, 255, 0.8)" }}>Currency Table</h1>
+                <table>
+                    <tr>
+                        <th colSpan='2'>
+                            <select value={selected.slice(3, 6)} onChange={(e) => setSelected("USD" + e.target.value)}>
+                                {arr.map((ele) => { return <option>{ele.slice(3, 6)}</option> })}
+                            </select>
+                        </th>
+                    </tr>
+                    {arr.map((e) => { return <tr><td>{e.slice(3, 6)}</td><td>{(data.quotes[e] / data.quotes[selected]).toFixed(5)}</td></tr> })}
+                </table>
+            </div>}
         </div>
     );
 }
